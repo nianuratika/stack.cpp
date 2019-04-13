6fb805a22cacfee334cede61250ea2704c6671a0
nianuratika <48706479+nianuratika@users.noreply.github.com>
2019-04-13 23:36:58 +0700

    Update stack.cpp

--- stack.cpp
+++ stack.cpp
@@ -1,1 +1,23 @@
 // nia nur atika 1817051015
+
+
+#include<iostream>
+#include<stdlib.h>
+#include<cstring>
+#include<windows.h>
+#define max 20
+using namespace std;
+
+struct tong
+{
+        int top;
+        char tmp[20][max];
+}
+tong;
+       
+void push (char sampah[20]);
+void pop();
+void awal();
+int isEmpty();
+int isFull();
+       
